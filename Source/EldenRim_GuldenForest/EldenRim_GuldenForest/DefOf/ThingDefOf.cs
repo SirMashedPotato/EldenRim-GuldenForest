79e4cd507d18ca6642d65b22e8ae171ffa2b22f3
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-04-11 11:13:30 +1200

    Gulden Ambrosia

---
+++ Source/EldenRim_GuldenForest/EldenRim_GuldenForest/DefOf/ThingDefOf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace EldenRim_GuldenForest
+{
+	[DefOf]
+	public static class ThingDefOf
+	{
+
+		static ThingDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(ThingDefOf));
+		}
+
+		public static ThingDef Plant_GuldenAmbrosia;
+	}
+}
